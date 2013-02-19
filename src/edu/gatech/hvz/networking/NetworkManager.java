09abb5fd3bd5baf3b9a40b010af8b8752955e512
Harrison Wright <wright8191@gmail.com>
2013-02-18 23:58:29 -0500

    changes to http call using JSoup

--- src/edu/gatech/hvz/networking/NetworkManager.java
+++ src/edu/gatech/hvz/networking/NetworkManager.java
@@ -1,22 +1,28 @@
 package edu.gatech.hvz.networking;
 
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLConnection;
+import java.util.Map;
+
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
 
 public class NetworkManager {
+	
+	private Map<String, String> casCookies;
+	
+	public NetworkManager()
+	{
+		
+	}
 
 	public Object makeRequest(String urlString) {
 		
 		try {
-			/* IDK HOW TO MAKE HTTP CALLS AND GET JSON BACK OR WHATEVER STILL LOOKING AROUND */
-			URL url = new URL(urlString);
-			URLConnection connection = url.openConnection();
-			connection.setDoInput(true);
-			connection.setDoOutput(true);
-		} catch (IOException e) {
+			Response r = Jsoup.connect(urlString).cookies(casCookies).method(Method.GET).execute();
+		} catch (IOException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e1.printStackTrace();
 		}
 		return null;
 	}
