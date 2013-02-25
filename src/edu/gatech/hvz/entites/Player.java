571315452371fb39c4d58b94119cf08e4dc78ab1
Jeremy <jgrebnergt@gmail.com>
2013-02-24 20:32:16 -0500

    Add fields to the Player entity

--- src/edu/gatech/hvz/entites/Player.java
+++ src/edu/gatech/hvz/entites/Player.java
@@ -1,10 +1,21 @@
 package edu.gatech.hvz.entites;
 
 public class Player {
+	private int id;
+	private String gt_name;
+	private String player_code;
+	private String fname, lname;
+	private String faction;
+	private String slogan;
+	private String starve_time;
+	private int kills;
 	
-	public Player()
-	{
-		
+	@Override
+	public String toString() {
+		return "Player [id=" + id + ", gt_name=" + gt_name + ", player_code="
+				+ player_code + ", fname=" + fname + ", lname=" + lname
+				+ ", faction=" + faction + ", slogan=" + slogan
+				+ ", starve_time=" + starve_time + ", kills=" + kills + "]";
 	}
-
+	
 }
