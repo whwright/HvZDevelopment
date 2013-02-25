1bcb0d1cdb15d49c99722199bae9dc2853d5ce1b
Jeremy <jgrebnergt@gmail.com>
2013-02-24 20:31:25 -0500

    Make ResourceManager a singleton

--- src/edu/gatech/hvz/ResourceManager.java
+++ src/edu/gatech/hvz/ResourceManager.java
@@ -1,18 +1,33 @@
 package edu.gatech.hvz;
 
+import edu.gatech.hvz.datasource.DataSourceManager;
 import edu.gatech.hvz.networking.NetworkManager;
 
 public class ResourceManager {
 	
+	private static ResourceManager resourceManager;
 	private NetworkManager networkManager;
+	private DataSourceManager dataManager;
 	
-	public ResourceManager()
+	private ResourceManager()
 	{
 		this.networkManager = new NetworkManager();
+		this.dataManager = new DataSourceManager();
+	}
+	
+	public static ResourceManager getResourceManager() {
+		if (resourceManager == null) {
+			resourceManager = new ResourceManager();
+		}
+		return resourceManager;
 	}
 
 	public NetworkManager getNetworkManager() {
 		return networkManager;
+	}
+	
+	public DataSourceManager getDataManager() {
+		return dataManager;
 	}
 
 }
