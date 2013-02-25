d33e64e23f9d8abd1c719f94d5e4676fef3f1704
Jeremy <jgrebnergt@gmail.com>
2013-02-24 20:30:13 -0500

    Add a class representing a kill

---
+++ src/edu/gatech/hvz/entites/Kill.java
@@ -0,0 +1,15 @@
+package edu.gatech.hvz.entites;
+
+public class Kill {
+	private String victim;
+	private int lat, lng;
+	private String time;
+	
+	@Override
+	public String toString() {
+		return "Kill [victim=" + victim + ", lat=" + lat + ", lng=" + lng
+				+ ", time=" + time + "]";
+	}
+	
+
+}
