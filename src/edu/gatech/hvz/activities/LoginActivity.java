0589acc0d983ed8757294a52ea7c1ee1e73db468
Jeremy <jgrebnergt@gmail.com>
2013-03-23 06:16:52 -0400

    Add a small amount of code to Login to make it easy to bypass the login screen

--- src/edu/gatech/hvz/activities/LoginActivity.java
+++ src/edu/gatech/hvz/activities/LoginActivity.java
@@ -26,6 +26,11 @@ public class LoginActivity extends Activity {
 		
 		resources = ResourceManager.getResourceManager();
 		
+		//Uncomment this to bypass logging in for basic testing
+		//Set the intent to whatever
+		startActivity(new Intent(LoginActivity.this, LandingPageActivity.class));
+		finish();
+		
 		loginButton = (Button) findViewById(R.id.button_login);
 		loginButton.setOnClickListener(new Button.OnClickListener() {
 	   		 public void onClick(View v) {
@@ -78,5 +83,5 @@ public class LoginActivity extends Activity {
 			}
 		}
 	 }
-
+	
 }
