de84948a9f401c5c46435311c1e379d9c537395b
Andrew Kim <andrewkim823@gmail.com>
2013-03-29 00:10:35 -0400

    Demo GUI
    I added some menu options to navigate to certain pages for demo.

--- src/edu/gatech/hvz/activities/MissionList.java
+++ src/edu/gatech/hvz/activities/MissionList.java
@@ -3,7 +3,9 @@ package edu.gatech.hvz.activities;
 import edu.gatech.hvz.R;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MissionList extends Activity {
 
@@ -16,8 +18,19 @@ public class MissionList extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_mission_list, menu);
+		getMenuInflater().inflate(R.menu.landing_page, menu);
+		menu.add(0,0,0,"Show Detail");
 		return true;
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item){
+		switch (item.getItemId()){
+		case 0:
+			Intent lineIntent = new Intent(MissionList.this,MissionDetail.class);
+			startActivity(lineIntent);
+			return true;
+		}
+		return false;
 	}
 
 }
