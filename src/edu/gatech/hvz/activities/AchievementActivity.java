8dcae839fbec0efd1380e2b680d1589bd7bfc559
Jeremy <jgrebnergt@gmail.com>
2013-04-17 22:18:16 -0400

    Add a basic Task to grab your achievements from the server and log them

--- src/edu/gatech/hvz/activities/AchievementActivity.java
+++ src/edu/gatech/hvz/activities/AchievementActivity.java
@@ -1,16 +1,34 @@
 package edu.gatech.hvz.activities;
 
+import java.util.Arrays;
+import java.util.Map;
+
 import edu.gatech.hvz.R;
+import edu.gatech.hvz.ResourceManager;
+import edu.gatech.hvz.entities.Achievement;
+import edu.gatech.hvz.entities.Player;
+import edu.gatech.hvz.networking.CASAuthenticator;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences.Editor;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class AchievementActivity extends Activity {
-
+	
+	private ResourceManager resources;
+	private AsyncTask<Void, Void, Boolean> task;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_achievement);
+		
+		resources = ResourceManager.getResourceManager();
+		
+		task = new AchievementTask().execute();
 	}
 
 	@Override
@@ -23,5 +41,33 @@ public class AchievementActivity extends Activity {
 		menu.add(Menu.NONE, 2, 2, "Logout");
 		return true;
 	}
+	
+	protected void onPause() {
+		super.onPause();
+		if (task != null && task.getStatus() == AsyncTask.Status.RUNNING) {
+			task.cancel(true);
+		}
+	}
+	
 
+	private class AchievementTask extends AsyncTask<Void, Void, Boolean> {
+		private Achievement[] achievements;
+		
+		protected Boolean doInBackground(Void ... voids) {
+			try {
+				achievements = resources.getDataManager().getAchievements();
+				return true;
+			} catch (Exception e) {
+				return false;
+			}
+		}
+
+		protected void onPostExecute(Boolean success) {
+			if (success) {
+				Log.i("AchievementActivity", Arrays.toString(achievements));
+			} else {
+				Toast.makeText(AchievementActivity.this, "There was an error fetching your achievements", Toast.LENGTH_LONG).show();
+			}
+		}
+	}
 }
