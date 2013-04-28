26b525f3d7279f61995d566659b7b0056ec7efd6
Jeremy <jgrebnergt@gmail.com>
2013-04-27 20:31:31 -0400

    Add an about page

---
+++ HvZAndroidClient/src/edu/gatech/hvz/activities/AboutActivity.java
@@ -0,0 +1,44 @@
+package edu.gatech.hvz.activities;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+import edu.gatech.hvz.R;
+import android.os.Bundle;
+
+/**
+ * A simple activity to display an About page.  This contains
+ * contact information, as well as licenses for software used.
+ */
+public class AboutActivity extends SherlockActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_profile);
+		
+		// ActionBar setup
+		ActionBar bar = getSupportActionBar();
+		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+		bar.setDisplayHomeAsUpEnabled(true);
+		bar.setTitle(getString(R.string.title_activity_about));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()){
+			case android.R.id.home:
+				finish();
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+}
